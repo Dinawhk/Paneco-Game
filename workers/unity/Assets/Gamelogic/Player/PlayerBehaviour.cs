af7e47f79ec2d49bb994384ab4fffe191831d9d0
Vincenzo Apetino <apetino.vincenzo94@gmail.com>
2017-11-08 10:41:34 +0100

    Animations fixed: they works on server side too now.

--- workers/unity/Assets/Gamelogic/Player/PlayerBehaviour.cs
+++ workers/unity/Assets/Gamelogic/Player/PlayerBehaviour.cs
@@ -7,33 +7,63 @@ using Improbable.Worker;
 using UnityEngine;
 using Improbable.Player;
 
+[WorkerType(WorkerPlatform.UnityClient)]
 public class PlayerBehaviour : MonoBehaviour
 	{
+	
+	[Require] private PlayerInput.Reader PlayerInputReader;
 	Animator anim;
+
+
+	private void OnEnable()
+	{
+		PlayerInputReader.MovementTriggered.Add (OnMovement);
+		PlayerInputReader.IdleTriggered.Add (OnIdle);
+	}
+
+	private void OnDisable()
+	{
+		PlayerInputReader.MovementTriggered.Remove (OnMovement);
+		PlayerInputReader.IdleTriggered.Remove (OnIdle);
+
+	}
 
 	void Awake () 
 	{
 		anim = GetComponent<Animator> ();
+		anim.SetBool ("IsRunning", false);
+
 	}
 
-	void FixedUpdate () 
+	private void OnMovement(Movement movement)
+	{
+		anim.SetBool ("IsRunning", true);
+	}
+
+	private void OnIdle(Idle idle)
+	{
+		anim.SetBool ("IsRunning", false);
+	}
+
+	/*void FixedUpdate () 
 	{
 		// Store the input axes.
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 
-		// Turn the player to face the mouse cursor.
-		Turning (h,v);
-	}
+		Turning (h, v);
+	}*/
 
-	void Turning(float x, float y)
+
+	/* void Turning(float x, float y)
 	{
 		// Create a boolean that is true if either of the input axes is non-zero.
 		bool walking = x != 0f || y != 0f;
 
 		// Tell the animator whether or not the player is walking.
 		anim.SetBool ("IsRunning", walking);
-	}
+	}*/
+			 
 
     void OnTriggerEnter(Collider other)
     {
--- workers/unity/Assets/Gamelogic/Player/PlayerInputSender.cs
+++ workers/unity/Assets/Gamelogic/Player/PlayerInputSender.cs
@@ -5,22 +5,31 @@ using Improbable.Player;
 
 
 namespace Assets.Gamelogic.Player {
-[WorkerType(WorkerPlatform.UnityClient)]
 
+[WorkerType(WorkerPlatform.UnityClient)]
 public class PlayerInputSender : MonoBehaviour
 {
 
     [Require] private PlayerInput.Writer PlayerInputWriter;
-
+			
+		private bool walking;
 
     void Update ()
     {
         var xAxis = Input.GetAxis("Horizontal");
         var yAxis = Input.GetAxis("Vertical");
-		
+		walking = xAxis != 0f || yAxis != 0f;
+
+			if (walking) {
+				PlayerInputWriter.Send (new PlayerInput.Update ().AddMovement (new Movement ()));	
+			} else {			
+				PlayerInputWriter.Send (new PlayerInput.Update ().AddIdle (new Idle ()));
+			}
+
         var update = new PlayerInput.Update();
         update.SetJoystick(new Joystick(xAxis, yAxis));
         PlayerInputWriter.Send(update);
+
     }
 
 
